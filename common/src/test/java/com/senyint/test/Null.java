3627891e8fb43a51b339f74a4cb79ea4c6eeef8c
liuhongming <liuhongming@senyint.com>
2017-07-12 17:12:18 +0800

    @fix null 可以转换成任何类型

---
+++ common/src/test/java/com/senyint/test/Null.java
@@ -0,0 +1,13 @@
+package com.senyint.test;
+
+public class Null {
+
+    public static void haha() {
+        System.out.println("haha");
+    }
+
+    public static void main(String[] args) {
+        ((Null) null).haha();
+    }
+
+}
