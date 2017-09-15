f51a43be1419a8f109064f29b71f8a889ac6eeb2
liuhongming <jason.liuhongming@gmail.com>
2017-09-15 17:33:43 +0800

    @fix 测试

--- pay-startup/src/main/java/com/senyint/pay/web/vo/BaseVo.java
+++ pay-startup/src/main/java/com/senyint/pay/web/vo/BaseVo.java
@@ -1,10 +1,6 @@
 package com.senyint.pay.web.vo;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.Serializable;
 import java.util.Date;
